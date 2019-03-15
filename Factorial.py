5541d5866f2ded81a426dbd407b1f80358dc2cc0
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2019-03-15 16:04:11 +0000

    Cleaned up and made more interactive

--- Factorial.py
+++ Factorial.py
@@ -1,17 +1,26 @@
-# def stupid(num):
-#     if num==10:
-#         return
-#     else:
-#         print('our number is', num)
-#         stupid(num+1)
-
-#stupid(0)
-
 def factorial(num):
     if num == 0:
         return 1
     else:
         return num*factorial(num-1)
 
-ans = factorial(12)
-print(ans)
+def main():
+    print("Welcome to your factorial calculator!")
+
+    while True:
+
+        num = int(input("Enter a positive number: "))
+
+        if num < 0:
+            print("Factorial does not exist for negative numbers, try again.")
+            main()
+        else:
+            ans = factorial(num)
+            print(str(num) + "! = " + str(ans))
+            again = input("Go again? (y/n) ")
+            if again == "y":
+                True
+            else:
+                break
+
+main()
